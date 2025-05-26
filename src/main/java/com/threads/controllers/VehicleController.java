209e4765a3bfc2db0b1911c25d004991bc0e8589
BrunoEspindola <brunoespindola.work@gmail.com>
2025-05-25 21:24:33 -0300

    feat: add strategy methods

--- src/main/java/com/threads/controllers/VehicleController.java
+++ src/main/java/com/threads/controllers/VehicleController.java
@@ -48,16 +48,29 @@ public class VehicleController extends Thread {
 					// Calculate full path through the crossing
 					List<Position> path = calculateCrossingPath(nextPosition);
 
+					System.out.println("Crossing path:");
+					for (Position pos : path) {
+						System.out.println(" - " + pos);
+					}
+
 					// Try to reserve all positions in the crossing path
-					if (!reserveFullPath(path)) {
+					if (reserveFullPath(path)) {
+
+						for (Position pos : path) {
+							System.out.println("X: " + pos.toString());
+							moveVehicle(pos);
+							sleep(vehicle.getSpeed());
+						}
+
+						crossingPath.clear();
+					} else {
+						System.out.println("Lá");
 						// If reservation fails, wait and retry in next iteration
 						sleep(vehicle.getSpeed());
 						continue;
 					}
-				}
 
-				// Try to acquire the next position
-				if (mutualExclusion.tryAcquire(nextPosition)) {
+				} else if (mutualExclusion.tryAcquire(nextPosition)) {
 					// If successful, move the vehicle to new position
 					moveVehicle(nextPosition);
 				}
@@ -85,41 +98,183 @@ public class VehicleController extends Thread {
 		return nextType.isCross();
 	}
 
-	private List<Position> crossingFromDownToLeft(Position entryPosition) {
+	private List<Position> crossingUpToUp(Position entryPosition) {
 		List<Position> path = new ArrayList<>();
-		Position current = entryPosition;
+		path.add(entryPosition);
 
-		path.add(current);
+		Position up1 = roadMap.getPositionAtUpFrom(entryPosition);
+		path.add(up1);
 
-		Position position2 = roadMap.getPositionAtDownFrom(current);
-		path.add(position2);
-		current = position2;
+		Position up2 = roadMap.getPositionAtUpFrom(up1);
+		path.add(up2);
 
-		Position position3 = roadMap.getPositionAtLeftFrom(position2);
-		path.add(position3);
-		current = position3;
+		return path;
+	}
+
+	private List<Position> crossingUpToRight(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position right = roadMap.getPositionAtRightFrom(entryPosition);
+		path.add(right);
+
+		return path;
+	}
+
+	private List<Position> crossingUpToLeft(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position up = roadMap.getPositionAtLeftFrom(entryPosition);
+		path.add(up);
+
+		Position left1 = roadMap.getPositionAtLeftFrom(up);
+		path.add(left1);
+
+		Position left2 = roadMap.getPositionAtLeftFrom(left1);
+		path.add(left2);
+
+		return path;
+	}
+
+	private List<Position> crossingDownToLeft(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position left = roadMap.getPositionAtLeftFrom(entryPosition);
+		path.add(left);
+
+		return path;
+	}
+
+	private List<Position> crossingDownToRight(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position down = roadMap.getPositionAtDownFrom(entryPosition);
+		path.add(down);
+
+		Position right1 = roadMap.getPositionAtRightFrom(down);
+		path.add(right1);
+
+		Position right2 = roadMap.getPositionAtRightFrom(right1);
+		path.add(right2);
+
+		return path;
+	}
+
+	private List<Position> crossingDownToUp(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position down1 = roadMap.getPositionAtUpFrom(entryPosition);
+		path.add(down1);
+
+		Position down2 = roadMap.getPositionAtUpFrom(down1);
+		path.add(down2);
+
+		return path;
+	}
+
+	private List<Position> crossingRightToDown(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position down = roadMap.getPositionAtDownFrom(entryPosition);
+		path.add(down);
+
+		return path;
+	}
+
+	private List<Position> crossingRightToLeft(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position right1 = roadMap.getPositionAtLeftFrom(entryPosition);
+		path.add(right1);
+
+		Position right2 = roadMap.getPositionAtLeftFrom(right1);
+		path.add(right2);
+
+		return path;
+	}
+
+	private List<Position> crossingRightToUp(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position right1 = roadMap.getPositionAtRightFrom(entryPosition);
+		path.add(right1);
+
+		Position up1 = roadMap.getPositionAtUpFrom(right1);
+		path.add(up1);
+
+		Position up2 = roadMap.getPositionAtUpFrom(up1);
+		path.add(up2);
+
+		return path;
+	}
+
+	private List<Position> crossingLeftToUp(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position up = roadMap.getPositionAtUpFrom(entryPosition);
+		path.add(up);
+
+		return path;
+	}
+
+	private List<Position> crossingLeftToRight(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position left1 = roadMap.getPositionAtLeftFrom(entryPosition);
+		path.add(left1);
+
+		Position left2 = roadMap.getPositionAtLeftFrom(left1);
+		path.add(left2);
+
+		return path;
+	}
+
+	private List<Position> crossingLeftToDown(Position entryPosition) {
+		List<Position> path = new ArrayList<>();
+		path.add(entryPosition);
+
+		Position left = roadMap.getPositionAtLeftFrom(entryPosition);
+		path.add(left);
+
+		Position down1 = roadMap.getPositionAtDownFrom(left);
+		path.add(down1);
+
+		Position down2 = roadMap.getPositionAtDownFrom(down1);
+		path.add(down2);
 
 		return path;
 	}
 
 	private List<Position> calculateCrossingPath(Position entryPosition) {
 		List<Position> path = new ArrayList<>();
-		int option = random.nextInt(3);
-		
+		int option = 0;
+
 		switch (entryPosition.getPositionType()) {
 		case ROAD_UP:
 			switch (option) {
 			case 0:
-				// fromUpToRight
+				// UpToRight
 				System.out.println("FromUpToRight");
+				path = crossingUpToRight(entryPosition);
 				break;
 			case 1:
-				// fromUpToUp
+				// UpToUp
 				System.out.println("FromUpToUp");
+				path = crossingUpToUp(entryPosition);
 				break;
 			case 2:
-				// fromUpToLeft
+				// UpToLeft
 				System.out.println("FromUpToLeft");
+				path = crossingUpToLeft(entryPosition);
 				break;
 			}
 			break;
@@ -127,20 +282,19 @@ public class VehicleController extends Thread {
 		case ROAD_DOWN:
 			switch (option) {
 			case 0:
-				// fromDownToLeft
+				// DownToLeft
 				System.out.println("FromDownToLeft");
-				path = crossingFromDownToLeft(entryPosition);
-
+				path = crossingDownToLeft(entryPosition);
 				break;
 			case 1:
-				// fromDownToDown
-				System.out.println("FromDownToDown");
-				path = crossingFromDownToLeft(entryPosition);
-
+				// DownToUp
+				System.out.println("FromDownToUp");
+				path = crossingDownToUp(entryPosition);
 				break;
 			case 2:
-				// fromDownToRight
+				// DownToRight
 				System.out.println("FromDownToRight");
+				path = crossingDownToRight(entryPosition);
 				break;
 			}
 			break;
@@ -148,16 +302,19 @@ public class VehicleController extends Thread {
 		case ROAD_RIGHT:
 			switch (option) {
 			case 0:
-				// fromRightToDown
+				// RightToDown
 				System.out.println("FromRightToDown");
+				path = crossingRightToDown(entryPosition);
 				break;
 			case 1:
-				// fromRightToRight
-				System.out.println("FromRightToRight");
+				// RightToLeft
+				System.out.println("FromRightToLeft");
+				path = crossingRightToLeft(entryPosition);
 				break;
 			case 2:
-				// fromRightToUp
+				// RightToUp
 				System.out.println("FromRightToUp");
+				path = crossingRightToUp(entryPosition);
 				break;
 			}
 			break;
@@ -165,16 +322,19 @@ public class VehicleController extends Thread {
 		case ROAD_LEFT:
 			switch (option) {
 			case 0:
-				// fromLeftToUp
+				// LeftToUp
 				System.out.println("FromLeftToUp");
+				path = crossingLeftToUp(entryPosition);
 				break;
 			case 1:
-				// fromLeftToLeft
-				System.out.println("FromLeftToLeft");
+				// LeftToRight
+				System.out.println("FromLeftToRight");
+				path = crossingLeftToRight(entryPosition);
 				break;
 			case 2:
-				// fromLeftToDown
+				// LeftToDown
 				System.out.println("FromLeftToDown");
+				path = crossingLeftToDown(entryPosition);
 				break;
 			}
 			break;
@@ -183,11 +343,6 @@ public class VehicleController extends Thread {
 			// Log ou tratamento para tipos inesperados
 			System.out.println("Unknown Segment Type: " + entryPosition.getPositionType());
 			break;
-		}
-
-		System.out.println("Crossing path:");
-		for (Position pos : path) {
-			System.out.println(" - " + pos);
 		}
 
 		return path;
@@ -215,10 +370,6 @@ public class VehicleController extends Thread {
 		mutualExclusion.release(current);
 		vehicle.setCurrentPosition(newPos);
 		notifySSE(vehicle);
-
-		if (crossingPath != null && !crossingPath.isEmpty() && crossingPath.get(0).equals(newPos)) {
-			crossingPath.remove(0);
-		}
 	}
 
 }
